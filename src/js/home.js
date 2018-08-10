75b3f08cc19bf821cf509c200863dc44d73d8b8e
Rene Ismael Barrios Rojas <ismaeljdz7@iMac-de-Rene.local>
2018-08-09 23:53:51 -0500

    Use of promises Js

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,28 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUser = new Promise(function(resolve, reject) {
+    setTimeout(function () {
+      reject();
+    }, 3000)
+});
+
+getUser
+  .then(function () {
+    console.log('hola todo esta ok')
+  })
+  .catch(function () {
+    console.log('todo fallo :( ');
+  })
+
+  Promise.all([
+    getUser,
+    getUser,
+  ])
+    .then(function () {
+      console.log('todo esta ok')
+    })
+    .catch(function () {
+      console.log('fallo promise all');
+    })
